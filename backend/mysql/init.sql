fc3154d256607815d9874d2b739963d01311d127
Ethan Potthoff <ethanpotthoff@Ethans-MacBook-Pro-2.local>
2020-04-07 17:39:18 -0500

    working to create tables

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -1,16 +1,72 @@
-CREATE TABLE IF NOT EXISTS `foogle`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+-- CREATE TABLE IF NOT EXISTS `foogle`.`test_table` (
+--     `id` INT NOT NULL AUTO_INCREMENT, 
+--     `value` VARCHAR(45), 
+--     PRIMARY KEY (`id`), 
+--     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+-- );
+
+-- INSERT INTO `foogle`.`test_table` (`value`) VALUES ('Sample Value');
+
+-- CREATE IF NOT EXISTS USER 'user'@'%' IDENTIFIED BY 'password';
+
+-- GRANT ALL PRIVILEGES ON foogle.* TO 'user'@'%';
+
+-- ALTER USER 'user'@'%' IDENTIFIED WITH MYSQL_NATIVE_PASSWORD BY 'password';
+
+-- FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS Foogle_DB;
+USE Foogle_DB;
+
+CREATE TABLE ingredients (
+ingredientId int,
+numberOfServings int,
+recipeId int
 );
 
-INSERT INTO `foogle`.`test_table` (`value`) VALUES ('Sample Value');
+DROP TABLE IF EXISTS `foods`;
+CREATE TABLE foods (
+foodId int PRIMARY KEY AUTO_INCREMENT,
+foodName varchar(50),
+foodGroupId int,
+totalCalories int,
+totalCaloriesFromFat int,
+totalFat int,
+transFat int,
+saturatedFat int,
+cholesterol int,
+sodium int,
+totalCarbohydrate int,
+dietaryFat int,
+sugars int,
+protein int
+);
 
-CREATE IF NOT EXISTS USER 'user'@'%' IDENTIFIED BY 'password';
+DROP TABLE IF EXISTS `foodGroups`;
+CREATE TABLE foodGroups (
+id int PRIMARY KEY AUTO_INCREMENT,
+foodGroup varchar(50)
+);
 
-GRANT ALL PRIVILEGES ON foogle.* TO 'user'@'%';
+DROP TABLE IF EXISTS `recipes`;
+CREATE TABLE recipes (
+recipeId int PRIMARY KEY AUTO_INCREMENT,
+userId int
+);
 
-ALTER USER 'user'@'%' IDENTIFIED WITH MYSQL_NATIVE_PASSWORD BY 'password';
+DROP TABLE IF EXISTS `savedFoods`;
+CREATE TABLE savedFoods (
+userId int,
+foodId int
+);
 
-FLUSH PRIVILEGES;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
+userId int PRIMARY KEY AUTO_INCREMENT,
+firstName varchar(50),
+lastName varchar(50),
+emailAddress varchar(50),
+username varchar(50),
+password varchar(50),
+isAdmin boolean
+);
