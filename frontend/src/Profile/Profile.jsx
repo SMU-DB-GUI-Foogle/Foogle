eb8c17a184b59da040f6caec82b11701c75f8da0
Jayson Werth <46936772+jayjayjetpain@users.noreply.github.com>
2020-04-21 13:17:59 -0500

    Added likes and dislikes functionality

--- frontend/src/Profile/Profile.jsx
+++ frontend/src/Profile/Profile.jsx
@@ -19,8 +19,8 @@ export const Profile = props => {
             <div className="col-6 card">
                 <h2 className="card-header">{account.firstName} {account.lastName}</h2>
                 <div className="card-body">
-                    {/* <h4>Likes({account.likes.length})</h4>
-                    <h4>Dislikes({account.dislikes.length})</h4>*/}
+                    <h4>Likes({props.likes.length})</h4>
+                    <h4>Dislikes({props.dislikes.length})</h4>
                     <h4>Saved({props.saves.length})</h4>
                     {/* <h4>Groups({account.groups.length})</h4>
                     <h4>Recipes({account.recipes.length})</h4>  */}
--- frontend/src/Profile/ProfileView.jsx
+++ frontend/src/Profile/ProfileView.jsx
@@ -12,7 +12,9 @@ export class ProfileView extends React.Component {
 
     state = {
         email: '',
-        saves: []
+        saves: [],
+        likes: [],
+        dislikes: []
     }
 
     deleteAccount() {
@@ -48,6 +50,34 @@ export class ProfileView extends React.Component {
         }
     }
 
+    deleteLiked(foodName) {
+        if(window.confirm("Are you sure you want to delete this liked product from your profile?")) {
+            let account = JSON.parse(sessionStorage.getItem('account'));
+            debugger;
+            this.profileRequests.deleteLikedProduct(account.userId, foodName)
+            .then(() => {
+                this.setState({ 
+                    likes: this.state.likes.filter(x => x.foodName !== foodName)
+                });
+                alert("Liked Product Removed");
+            });
+        }
+    }
+
+    deleteDisliked(foodName) {
+        if(window.confirm("Are you sure you want to delete this disliked product from your profile?")) {
+            let account = JSON.parse(sessionStorage.getItem('account'));
+            debugger;
+            this.profileRequests.deleteDislikedProduct(account.userId, foodName)
+            .then(() => {
+                this.setState({ 
+                    dislikes: this.state.dislikes.filter(x => x.foodName !== foodName)
+                });
+                alert("Disliked Product Removed");
+            });
+        }
+    }
+
     render() {
         if(this.state.redirect) {
             return (<Redirect to={ this.state.redirect }/>);
@@ -55,7 +85,7 @@ export class ProfileView extends React.Component {
 
         return <>
             <div className="jumbotron p-4">
-                <Profile saves={this.state.saves} />
+                <Profile saves={this.state.saves} likes={this.state.likes} dislikes={this.state.dislikes} />
                 <div className="row mt-2 mr-2">
                     <DropdownButton className="col-3" id="dropdown-basic-button" title="View Options">
                         <Dropdown.Item href={window.location.pathname + `/edit`}>Edit Profile</Dropdown.Item>
@@ -82,9 +112,9 @@ export class ProfileView extends React.Component {
                 </div>
             </div>
 
-            {/* <div className="card p-2 m-2">
-                <LikesView account={JSON.stringify(this.state.userAccount)} />
-            </div>  */}
+            <div className="card p-2 m-2">
+                <LikesView deleteLiked={name => this.deleteLiked(name)} likes={this.state.likes} deleteDisliked={name => this.deleteDisliked(name)} dislikes={this.state.dislikes} />
+            </div> 
             
             <div className="card p-2 m-2">
                 <SavedView deleteSaved={name => this.deleteSaved(name)} saves={this.state.saves} />
@@ -98,6 +128,10 @@ export class ProfileView extends React.Component {
         let account = JSON.parse(sessionStorage.getItem("account"));
         this.profileRequests.getAccountSaves(account.username, account.userId)
             .then(saves => this.setState({ saves }));
+        this.profileRequests.getAccountLikes(account.username, account.userId)
+            .then(likes => this.setState({ likes }));
+        this.profileRequests.getAccountDislikes(account.username, account.userId)
+            .then(dislikes => this.setState({ dislikes }));
     }
 
 }
