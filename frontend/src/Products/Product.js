80da8c565709fb4efe304e7c60f94524874c11db
manovjain <manovj@smu.edu>
2020-04-27 21:04:18 -0500

    Changes to index.css, product.js, and ProudctView.jsx

--- frontend/src/Products/Product.js
+++ frontend/src/Products/Product.js
@@ -7,16 +7,16 @@ export const Product = props => {
         
         <Card.Title id = "foodCard" className="card-header bg-light">Name: {props.product.foodName}</Card.Title>  
         <Card.Text id = "foodCard" className="list-group">
-            <div className="list-group-item px-3 py-2">Serving Portion: {props.product.servingPortion}</div>  
-            <div className="list-group-item px-3 py-2">Total Calories: {props.product.totalCalories}</div>  
-            <div className="list-group-item px-3 py-2">Total Fat: {props.product.totalFat}</div>  
-            <div className="list-group-item px-3 py-2">Trans Fat: {props.product.transFat}</div>  
-            <div className="list-group-item px-3 py-2">Saturated Fat: {props.product.saturatedFat}</div>  
-            <div className="list-group-item px-3 py-2">Cholesterol: {props.product.cholesterol}</div>  
-            <div className="list-group-item px-3 py-2">Sodium: {props.product.sodium}</div>  
-            <div className="list-group-item px-3 py-2">Total Carbohydrates: {props.product.totalCarbohydrate}</div>  
-            <div className="list-group-item px-3 py-2">Sugars: {props.product.sugars}</div>  
-            <div className="list-group-item px-3 py-2">Protein: {props.product.protein}</div>
+            <div id = "foodStat" className="list-group-item px-3 py-2">Serving Portion: {props.product.servingPortion}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Total Calories: {props.product.totalCalories}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Total Fat: {props.product.totalFat}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Trans Fat: {props.product.transFat}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Saturated Fat: {props.product.saturatedFat}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Cholesterol: {props.product.cholesterol}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Sodium: {props.product.sodium}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Total Carbohydrates: {props.product.totalCarbohydrate}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Sugars: {props.product.sugars}</div>  
+            <div id = "foodStat" className="list-group-item px-3 py-2">Protein: {props.product.protein}</div>
         </Card.Text>
     </Card>
 
